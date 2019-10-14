3c66cf996018e90088a74545b05e4f7ec51280ce
tsepa0 <tsepa.stepan@gmail.com>
2019-10-14 19:31:11 +0300

    initial docker files

---
+++ examples/basic_example.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Basic example, get stop info and save it to a file
+
+import json
+from yandex_transport_webdriver_api import YandexTransportProxy
+
+proxy = YandexTransportProxy('127.0.0.1', 25555)
+url = "https://yandex.ru/maps/213/moscow/?ll=37.742975%2C55.651185&masstransit%5BstopId%5D=stop__9647487&mode=stop&z=18"
+url2 = "https://yandex.ru/maps/213/moscow/?ll=37.463335%2C55.720286&masstransit%5BstopId%5D=stop__9650362&mode=masstransit&z=18"
+data = proxy.get_stop_info(url2)
+with open('data.json', 'w') as file:
+    file.write(json.dumps(data,indent=4, separators=(',', ': ')))
