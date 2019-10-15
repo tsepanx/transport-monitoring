9e299051e39895af0a62b12b67aef0390ba93366
tsepa0 <tsepa.stepan@gmail.com>
2019-10-15 21:11:46 +0300

    instructions to install docker in  docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -1,3 +1,7 @@
+# firstly install docker:
+# ubuntu : apt install docker
+# mac os, smth like : brew install docker
+
 docker pull owlsoul/ytproxy:latest
 docker run -it -p 25555:25555 owlsoul/ytproxy:latest
 
