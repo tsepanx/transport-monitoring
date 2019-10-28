07c45dd33b2e0b6ef90cc5c361cbc2e2f21570ad
tsepa0 <tsepa.stepan@gmail.com>
2019-10-28 10:26:35 +0300

    db initial file

---
+++ db_work.py
@@ -0,0 +1,11 @@
+from peewee import *
+
+db = SqliteDatabase('people.db')
+
+class Person(Model):
+    name = CharField()
+    birthday = DateField()
+    is_relative = BooleanField()
+    class Meta:
+        database = db
+
