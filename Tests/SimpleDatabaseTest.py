9a561a4874b7b053e0be089dbdc8213f1904d23c
tsepa0 <tsepa.stepan@gmail.com>
2019-12-12 23:35:29 +0300

    fixed tests

--- Tests/SimpleDatabaseTest.py
+++ Tests/SimpleDatabaseTest.py
@@ -3,8 +3,10 @@ from main import *
 
 
 class MyTestCase(unittest.TestCase):
-    def is_not_empty(self):
+    def test_is_not_empty(self):
         self.assertFalse(len(stop_data_dict) == 0)
+
+    
 
 
 if __name__ == '__main__':
